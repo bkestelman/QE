a9fc409ec1a33f3dd9f82f6f1929a5d7ac06d928
benito <bkestelman@outlook.com>
2018-02-03 18:32:23 -0500

    Sample results of fit_test_simple

---
+++ Fitting/fit_test_simple.py
@@ -0,0 +1,25 @@
+import numpy as np
+import scipy.optimize as optimization
+import matplotlib.pyplot as plt
+
+# Function to fit
+def func(x, a, b, c, d):
+    return a * np.sin(b*x+c) + d
+
+# Random noise
+def noise(amount):
+    return (np.random.random()-0.5)*amount
+
+# Generate data for function with noise
+xdata = np.array([n*np.pi/8 for n in range(8)])  
+ydata = np.array([np.sin(x)+noise(1) for x in xdata])
+
+# Fit data
+fit = optimization.curve_fit(func, xdata, ydata)
+print(fit) # Look at output from print to understand structure of fit object
+
+yfit = np.array(func(xdata, fit[0][0], fit[0][1], fit[0][2], fit[0][3])) # Fit function data
+
+plt.plot(xdata, ydata, 'ro') # Scatter plot of original, noisy data
+plt.plot(xdata, yfit) # Line plot of fit function
+plt.show()
