bfaa75131fcc9e493122ce019496a74814b88dea
benito <bkestelman@outlook.com>
2018-02-03 22:28:47 -0500

    Switch to code based logging

--- settings.py
+++ settings.py
@@ -1,43 +1,16 @@
+import logging
+
 LOG_DIR = 'Log'
 
 def log_path(filename):
-    return LOG_DIR + '/' + filename
+    return LOG_DIR + '/' + filename + '.log'
 
-LOG_CONFIG = {
-        'version': 1,
-        'formatters': {
-            'simple': {
-                'format': '%(asctime)s - %(name)s - %(levelname)s -\n%(message)s',
-                },
-            },
-        'handlers': {
-            'plot': {
-                'level': 'DEBUG',
-                'class': 'logging.FileHandler',
-                'filename': log_path('plot.log'),
-                'formatter': 'simple',
-                'mode': 'a',
-                },
-            'clean': {
-                'level': 'DEBUG',
-                'class': 'logging.FileHandler',
-                'filename': log_path('clean.log'),
-                'formatter': 'simple',
-                'mode': 'a',
-                },
-            },
-        'loggers': {
-            'plot': {
-                'handlers': ['plot'],
-                'propagate': False,
-                },
-            'clean': {
-                'handlers': ['clean'],
-                'propagate': False,
-                },
-            },
-        'root': {
-            'level': 'DEBUG',
-            },
-        }
-
+def createLogger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(log_path(name), 'a')
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
